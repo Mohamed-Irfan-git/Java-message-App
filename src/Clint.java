ea4ceb7ca5f62be9ca853b49fb6a510c12e5f39c
Mohamed.Irfan.git <irfanmt29@gmail.com>
2025-02-28 23:47:15 +0530

    commit

--- src/Clint.java
+++ src/Clint.java
@@ -57,7 +57,7 @@ public class Clint  extends JFrame implements Runnable, ActionListener {
 
     public static void main(String[] args) throws IOException {
        new Clint();
-       djfniigif
+
 
 
     }
